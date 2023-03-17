433a8523f44b258d121c36203de1e8835a18a687
esteban.bobbiesi <esteban.bobbiesi@kiuviservices.com.ar>
2023-03-17 07:54:29 -0300

    Added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -88,3 +89,19 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         Return string representation of our user
         """
         return str(self.email)
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+
+    # El primer argumento es el nombre del modelo que sera el main para esta foreign key
+    # El segundo argumento es on_delete,
+    # lo que hace es decirle a Django que hacer si el modelo main se remueve
+    # se remueva todo lo que este asociado a ese modelo
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        """Return the model as string"""
+        return str(self.status_text)
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -9,6 +9,8 @@ router.register("hello-viewset", views.HelloViewSet, base_name="hello-viewset")
 # No necesitamos establecer un nombre porque Django lo resuelve tomando el del modelo
 router.register("profile", views.UserProfileViewSet)
 
+router.register("feed", views.UserProfileFeedViewSet)
+
 urlpatterns = [
     path("hello-view/", views.HelloApiView.as_view()),
     path("login/", views.UserLoginApiView.as_view()),
